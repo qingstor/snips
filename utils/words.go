eef4e65844387968ec3806b77061d64c4dc3bdfd
Xuanwo <xuanwo@yunify.com>
2017-10-08 14:26:20 +0800

    utils: Add UUID into words map
    Signed-off-by: Xuanwo <xuanwo@yunify.com>

--- utils/words.go
+++ utils/words.go
@@ -72,6 +72,7 @@ var lowerCaseToCapitalizedWordsMap = map[string]string{
 	"uri":           "URI",
 	"url":           "URL",
 	"usb":           "USB",
+	"uuid":          "UUID",
 	"vcpus":         "VCPUs",
 	"vxnet":         "VxNet",
 	"vxnets":        "VxNets",
@@ -108,6 +109,7 @@ var abbreviateWordsMap = []string{
 	"TRX",
 	"UI",
 	"URL",
+	"UUID",
 	"VCPUs",
 	"VxNet",
 }
